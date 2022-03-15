708642b7b91cc96c30092796a554451a2584225f
Andrey Grytsenko <andreyapg@gmail.com>
2022-03-15 13:05:22 +0200

    Routing: navigation links and Post Create page 2022-03-15

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,11 +1,23 @@
 require('./bootstrap');
 
 import { createApp } from "vue";
+import { createRouter, createWebHistory } from 'vue-router';
+import App from './layouts/App';
 import PostsIndex from './components/Posts/Index';
+import PostsCreate from './components/Posts/Create';
 // https://github.com/gilbitron/laravel-vue-pagination
 import LaravelVuePagination from "laravel-vue-pagination";
 
-const app = createApp({});
-app.component('posts-index', PostsIndex);
+const routes = [
+    { path: '/', component: PostsIndex },
+    { path: '/posts/create', component: PostsCreate }
+];
+const router = createRouter({
+    history: createWebHistory(),
+    routes
+});
+
+const app = createApp(App);
+app.use(router); // app.component we delete because it will be a part of router
 app.component('Pagination', LaravelVuePagination);
 app.mount('#app');

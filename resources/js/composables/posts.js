1495011ea30f0ab23d59919cef249e5134106fac
Andrey Grytsenko <andreyapg@gmail.com>
2022-03-16 11:49:30 +0200

    Store new Post 2022-03-16

--- resources/js/composables/posts.js
+++ resources/js/composables/posts.js
@@ -1,7 +1,9 @@
 import { ref } from 'vue';
+import { useRouter } from 'vue-router';
 // use<> - default name conversion
 export default function usePosts() {
     const posts = ref({});
+    const router = useRouter();
 
     const getPosts = async (
         page = 1,
@@ -19,5 +21,12 @@ export default function usePosts() {
             });
     }
 
-    return { posts, getPosts }
+    const storePost = async (post) => {
+        await axios.post('http://pk-lv.local:8400/api/posts', post)
+            .then(response => {
+                router.push({name: 'posts.index'});
+            });
+    }
+
+    return { posts, getPosts, storePost }
 }

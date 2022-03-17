c223d5507ba2a29d7322436ec734ef64873f2e0d
Andrey Grytsenko <andreyapg@gmail.com>
2022-03-17 09:27:55 +0200

    Authenticated & Guest layouts added 2022-03-17

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,13 +1,12 @@
 require('./bootstrap');
 
 import { createApp } from "vue";
-import App from './layouts/App';
 // https://github.com/gilbitron/laravel-vue-pagination
 import LaravelVuePagination from "laravel-vue-pagination";
 import router from './routes/index';
 import VueSweetalert2 from "vue-sweetalert2";
 
-const app = createApp(App);
+const app = createApp({});
 app.use(router); // app.component we delete because it will be a part of router
 app.use(VueSweetalert2);
 app.component('Pagination', LaravelVuePagination);
--- resources/js/routes/index.js
+++ resources/js/routes/index.js
@@ -1,26 +1,44 @@
 import { createRouter, createWebHistory } from 'vue-router';
+import AuthenticatedLayout from "../layouts/Authenticated";
+import GuestLayout from "../layouts/Guest";
 import PostsIndex from "../components/Posts/Index";
 import PostsCreate from "../components/Posts/Create";
 import PostsEdit from "../components/Posts/Edit";
+import Login from "../components/Login";
 
 const routes = [
     {
-        path: '/',
-        component: PostsIndex,
-        name: 'posts.index',
-        meta: { title: 'Posts'}
+        component: GuestLayout,
+        children: [
+            {
+                path: '/login',
+                component: Login,
+                name: 'login'
+            }
+        ]
     },
     {
-        path: '/posts/create',
-        component: PostsCreate,
-        name:  'posts.create',
-        meta: { title: 'Add new post'}
-    },
-    {
-        path: '/posts/edit/:id',
-        component: PostsEdit,
-        name:  'posts.edit',
-        meta: { title: 'Edit post'}
+        component: AuthenticatedLayout,
+        children: [
+            {
+                path: '/posts',
+                component: PostsIndex,
+                name: 'posts.index',
+                meta: { title: 'Posts'}
+            },
+            {
+                path: '/posts/create',
+                component: PostsCreate,
+                name:  'posts.create',
+                meta: { title: 'Add new post'}
+            },
+            {
+                path: '/posts/edit/:id',
+                component: PostsEdit,
+                name:  'posts.edit',
+                meta: { title: 'Edit post'}
+            }
+        ]
     }
 ];
 
